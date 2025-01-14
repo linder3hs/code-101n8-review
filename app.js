d215f7e92a7ba0e0b8ea457ba79a1b4fa40db2a8
linder3hs <linderhassinger00@gmail.com>
2025-01-13 20:10:41 -0500

    feat: block generator

--- app.js
+++ app.js
@@ -1,3 +1,54 @@
+function generarColorHex() {
+  // Generamos un número aleatorio entre 0 y 16777215 (0xFFFFFF en hexadecimal)
+  const numeroAleatorio = Math.floor(Math.random() * 16777216);
+  // Convertimos el número a formato hexadecimal y aseguramos que tenga 6 dígitos
+  const colorHex = `#${numeroAleatorio.toString(16).padStart(6, "0")}`;
+  return colorHex;
+}
+
+// como busco un elemento por id
+const btnGame2 = document.getElementById("btnGame2");
+btnGame2.addEventListener("click", generarBloques);
+
+function generarBloques() {
+  // Busacando elementos por id con la funcion getElementById
+  const title = document.getElementById("title");
+  // podemos modificar el texto
+  title.textContent = "Hola fui modificado desde JS";
+  title.style.fontFamily = "Verdana";
+  title.style.color = "green";
+
+  // preguntar la cantidad de cuadrados
+  const cantidadDeCuadrados = parseInt(
+    prompt("Ingrese la cantidad de cuadrados")
+  );
+
+  // definir un contador para usarlo en el while
+  let contador = 0;
+
+  // estamos accediendi al body para colocar los cuadrados dentro de el
+  const body = document.body;
+
+  // creamos un while para ejecutar la creacion en base a la cantidad de veces
+  while (contador < cantidadDeCuadrados) {
+    // createElement: Permite crear una etiqueta html
+    const cuadrado = document.createElement("div"); // <div></div>
+    cuadrado.textContent = contador;
+    // cuales son las propiedades de un cuadrado?
+    cuadrado.style.width = "100px";
+    cuadrado.style.height = "100px";
+    cuadrado.style.backgroundColor = generarColorHex();
+    cuadrado.style.border = "1px solid";
+
+    // appendChild: sirve para agregar un elemento a un contenedor
+    body.appendChild(cuadrado);
+    contador++;
+  }
+}
+
+// title.style.display = "none";
+// solo busca a la primera coincidencia y obtiene el elemento
+
 /* Componente 1
       Al entrar a mi pagina web quiero que al usuario se le pregunta
       la edad, si el usuario es menor entonces vamos a mostrar
@@ -24,11 +75,11 @@
 //   );
 // }
 
-const backgrounColor = prompt(
-  "Ingese un color, este será el color de fondo de la pagina"
-);
-// // vamos a acceder al body
-document.body.style.backgroundColor = backgrounColor;
+// const backgrounColor = prompt(
+//   "Ingese un color, este será el color de fondo de la pagina"
+// );
+// // // vamos a acceder al body
+// document.body.style.backgroundColor = backgrounColor;
 
 function registrarHermanos() {
   // Me pregunta la cantidad de hermanos
